58980cbeb2de5d1b67c494dc69e88706fbac4fc5
Alieyeh <Alieyeh@gmail.com>
2022-12-15 05:11:09 +0330

    INIT: files added
    trial_design.py
    data_manipulation.py
    visualization.py

---
+++ data_manipulation.py
@@ -0,0 +1,22 @@
+def handle_null():
+    pass
+
+
+def change_type():
+    pass
+
+
+def merge():
+    pass
+
+
+def BMI():
+    pass
+
+
+def download():
+    pass
+
+
+def numeric_to_categorical():
+    pass
---
+++ visualization.py
@@ -0,0 +1,29 @@
+def cluster_3D():
+    pass
+
+
+def cluster_2D():
+    pass
+
+
+def graph_3D():
+    pass
+
+
+def graph():
+    pass
+
+
+def relation():
+    pass
+
+
+def time_to_death():
+    pass
+
+
+def survival_analysis():
+    pass
+
+
+
