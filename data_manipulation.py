04b0537384f3c2d10054eb26c14b3b54ca30c6cd
Jiayi Wang <13671769264@163.com>
2022-12-15 15:58:05 +0000

    INIT: add __init__ file

--- data_manipulation.py
+++ data_manipulation.py
@@ -6,12 +6,45 @@ def change_type():
     pass
 
 
-def merge():
-    pass
+def data_selection(cond=None, keep_col=None, drop_col=None, sort_by=None, merge_data=None,
+                   merge_by=None, merge_keep_col=None, sort_asc=True, rename=None, input_data=None):
+    if cond is not None:
+        output_data = input_data.query(cond)
+    else:
+        output_data = input_data
+
+    if keep_col is not None:
+        output_data = output_data.loc[:, keep_col]
+
+    if drop_col is not None:
+        output_data.drop(drop_col, axis=1, inplace=True)
+
+    if merge_keep_col is not None:
+        merge_data = merge_data.loc[:, merge_keep_col]
+
+    if merge_data is not None:
+        output_data = output_data.merge(merge_data, on=merge_by, how="left")
+
+    if sort_by is not None:
+        output_data.sort_values(by=sort_by, ascending=sort_asc, inplace=True)
+
+    if rename is not None:
+        output_data.rename(columns=rename, inplace=True)
+
+    return output_data
 
 
-def BMI():
-    pass
+def derive_baseline(input_data=None, chg=None, pchg=None, base_visit=None):
+    baseline = data_selection(keep_col=["USUBJID", "PARAMCD", "AVISITN", "AVAL"], input_data=input_data,
+                              cond=base_visit, rename={"AVAL":"BASE"})
+
+    input_data = data_selection(input_data=input_data, merge_data=baseline, merge_by=["USUBJID", "PARAMCD"])
+    if chg:
+        input_data["CHG"] = input_data["AVAL"]-input_data["BASE"]
+    if pchg:
+        input_data["PCHG"] = (input_data["AVAL"]-input_data["BASE"])/input_data["BASE"]
+
+    return input_data
 
 
 def download():
