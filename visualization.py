be2c3b174facd3c9e642be2d482aead04a8c9c5e
Alieyeh <Alieyeh@gmail.com>
2022-12-15 20:50:39 +0330

    Wrote graph

--- visualization.py
+++ visualization.py
@@ -1,3 +1,10 @@
+import numpy as np
+from matplotlib import pyplot as plt
+from matplotlib import style
+from mpl_toolkits.mplot3d import Axes3D
+import seaborn as sns
+
+
 def cluster_3D():
     pass
 
@@ -6,8 +13,25 @@ def cluster_2D():
     pass
 
 
-def graph_3D():
-    pass
+def graph_3D(df, ax1: str, ax2: str, ax3: str, lab1 = None,
+            lab2 = None, lab3 = None):
+    sns.set(style="whitegrid")
+    fig = plt.figure(figsize=(12, 12))
+    ax = fig.add_subplot(111, projection='3d')
+    if not (lab1 is None) and type(lab1) == str:
+        ax.set_xlabel(lab1)
+    else:
+        ax.set_xlabel(ax1)
+    if not (lab2 is None) and type(lab2) == str:
+        ax.set_ylabel(lab2)
+    else:
+        ax.set_ylabel(ax2)
+    if not (lab3 is None) and type(lab3) == str:
+        ax.set_zlabel(lab3)
+    else:
+        ax.set_zlabel(ax3)
+    ax.scatter(df[ax1], df[ax2], df[ax3], color='purple')
+    return ax, fig
 
 
 def graph():
