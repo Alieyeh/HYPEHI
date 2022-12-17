8f4dacc460610d3e7f6b3be3be719721726a895b
Jiayi Wang <13671769264@163.com>
2022-12-17 13:15:50 +0000

    update function longitudinal_graph()

--- visualization.py
+++ visualization.py
@@ -4,7 +4,8 @@ from matplotlib import style
 from mpl_toolkits.mplot3d import Axes3D
 import seaborn as sns
 import pandas as pd
-import textwrap as twp
+import scipy.stats
+import itertools
 from sklearn.cluster import OPTICS, cluster_optics_dbscan
 import matplotlib.gridspec as gridspec
 from sklearn.cluster import DBSCAN
@@ -19,7 +20,7 @@ def cluster_3D(df, cols, type, number, min_sample = 3, eps = 0.5,
         clusters = DBSCAN(eps = eps, min_samples = min_sample).fit(df[cols])
         df['Clusters'] = clusters.labels_
     else:
-
+        pass  # add to make code run, delete it when you see this
     sns.set(style="whitegrid")
     fig = plt.figure(figsize=(12, 12))
     ax = fig.add_subplot(111, projection='3d')
@@ -69,7 +70,36 @@ def graph_3D(df, ax1: str, ax2: str, ax3: str, lab1 = None,
     return fig, ax
 
 
-def demo_graph(var=None, group_by=None, input_data=None):
+def demo_graph(var: list, input_data: pd.DataFrame, group_by=None):
+    """Return a new matrix of given shape and type, without initializing entries.
+
+        Parameters
+        ----------
+        var : int or tuple of int
+            Shape of the empty matrix.
+        input_data : data-type, optional
+            Desired output data-type.
+        group_by : {'C', 'F'}, optional
+            Whether to store multi-dimensional data in row-major
+            (C-style) or column-major (Fortran-style) order in
+            memory.
+
+        Returns
+        -------
+        ret : ndarray
+        An array, or list of arrays, each with ``a.ndim >= 1``.
+        Copies are made only if necessary.
+
+        See Also
+        --------
+        atleast_2d, atleast_3d
+
+        Examples
+        --------
+        >>>
+        >>>    # filled with random data
+
+    """
     for col in var:
         fig, ax = plt.subplots(figsize=(15, 10))
         ax.tick_params(axis='both', which='major', labelsize=20)
@@ -106,18 +136,54 @@ def demo_graph(var=None, group_by=None, input_data=None):
         plt.ylabel(col, fontsize=16)
         plt.title(f"Plot and summary table for {col.title()}", fontsize=30)
         plt.show()
-        # what should I return?
+        return fig, ax
+        # what should I return?/ but I have multiple output plots
         #  axis and figure       
 
 
-def longitudinal_graph(y=None, x=None, by=None, input_data=None):
-    for col in x:
+def f_test(group1, group2):
+    x = np.array(group1)
+    y = np.array(group2)
+    if np.var(group2, ddof=1) != 0:
+        f_value = np.var(group1, ddof=1) / np.var(group2, ddof=1)
+        nun = x.size - 1
+        dun = y.size - 1
+        p_value = round(1 - scipy.stats.f.cdf(f_value, nun, dun), 3)
+    else:
+        p_value = np.nan
+    return p_value
+
+
+def longitudinal_graph(outcome, time, group_by, input_data=pd.DataFrame):
+
+    for col in outcome:
+        summary = round(input_data.groupby([group_by, time])[col].mean().reset_index(), 2)
         fig, ax = plt.subplots(figsize=(15, 10))
         ax.tick_params(axis='both', which='major', labelsize=20)
         sns.set_theme(font_scale=1.2, palette="Set2")
-        if np.issubdtype(input_data[col].dtype, np.number):
-            sns.boxplot(ax=ax, data=dat, y=col, x=group_by, orient="v")
+        sns.lineplot(x=time, y=col, hue=group_by, data=summary)
 
+        time_group = input_data[time].unique()
+        group = input_data[group_by].unique()
+        temp = pd.DataFrame()
+        for i in time_group:
+            for g1, g2 in itertools.combinations(range(len(group)), 2):
+                a = input_data.query(f"{time}=={i} and {group_by}=='{group[g1]}'")[col].transpose()
+                b = input_data.query(f"{time}=={i} and {group_by}=='{group[g2]}'")[col].transpose()
+                p_value = f_test(a, b)
+                row = pd.DataFrame([[i, f"p-value: \n {group[g1]} vs {group[g2]}", p_value]],
+                                   columns=["Time", "Compare", col])
+                temp = pd.concat([temp, row])
+        temp = temp.pivot_table(index='Compare', columns="Time")
+        print(summary)
+        summary = summary.pivot_table(index=group_by, columns=time)
+        summary = pd.concat([summary, temp])
+        ax.set(xlabel=None)
+        plt.table(cellText=summary.values, rowLabels=summary.index, loc='bottom', bbox=[0, -0.5, 1, 0.4], cellLoc="center")
+        plt.subplots_adjust(left=0.2, bottom=0.3)
+        plt.title(f"Line plot and summary table for {col.title()}", fontsize=30)
+        plt.show()
+    return fig, ax
 
 
 def relation():
